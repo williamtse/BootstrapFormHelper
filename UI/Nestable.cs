3c9c3b1da6819ca36f35c669539afea4d2321f61
xwengf <xwengf@126.com>
2019-07-16 20:51:06 +0800

    fix

--- UI/Nestable.cs
+++ UI/Nestable.cs
@@ -9,10 +9,10 @@ namespace BootstrapHtmlHelper.UI
     public class Nestable : UIBase
     {
         private List<Node> _nodes;
-        private string html="";
+        private string html = "";
         private string _id = "";
         private TreeNodeHandler _handle;
-        public Nestable(List<Node> nodes, string id, TreeNodeHandler nodeHandler=null)
+        public Nestable(List<Node> nodes, string id, TreeNodeHandler nodeHandler = null)
         {
             Builder builder = new Builder(nodes);
             _nodes = builder.getTree();
@@ -25,6 +25,49 @@ namespace BootstrapHtmlHelper.UI
             BuildTree(_nodes);
             html += "</ol></div>";
             return html;
+        }
+
+        public override string GetScript()
+        {
+            string script = ";";
+            script += "$('.tree_branch_delete').unbind('click').click(function () {"
+                    + "    var id = $(this).data('id');"
+                    + "    swal({"
+                    + "        title: \"确认删除 ? \","
+                    + "        type: \"warning\","
+                    + "        showCancelButton: true,"
+                    + "        confirmButtonColor: \"#DD6B55\","
+                    + "        confirmButtonText: \"确认\","
+                    + "        showLoaderOnConfirm: true,"
+                    + "        cancelButtonText: \"取消\","
+                    + "        preConfirm: function () {"
+                    + "            return new Promise(function (resolve) {"
+                    + "                $.ajax({"
+                    + "                    method: 'post',"
+                    + "                    url: '/admin/articles/' + id,"
+                    + "                    data: {"
+                    + "                        _method: 'delete',"
+                    + "                        _token: LA.token,"
+                    + "                    },"
+                    + "                    success: function (data) {"
+                    + "                        $.pjax.reload('#pjax-container');"
+                    + "                        resolve(data);"
+                    + "                    }"
+                    + "                });"
+                    + "            });"
+                    + "        }"
+                    + "    }).then(function (result) {"
+                    + "        var data = result.value;"
+                    + "        if (typeof data === 'object') {"
+                    + "            if (data.status) {"
+                    + "                swal(data.message, '', 'success');"
+                    + "            } else {"
+                    + "                swal(data.message, '', 'error');"
+                    + "            }"
+                    + "        }"
+                    + "    });"
+                    + "});";
+            return script;
         }
 
         private void BuildTree(List<Node> nodes)
